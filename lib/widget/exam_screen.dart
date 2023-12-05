68704bee176dedef9a39c960f1ae80b2d95b56dd
yejin <kimyejin0044@gmail.com>
2023-12-06 04:43:12 +0900

    GPT 연결

--- lib/widget/exam_screen.dart
+++ lib/widget/exam_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../../api/api_key.dart';
+import 'dart:convert';
 
 class ExamScreen extends StatefulWidget {
   final String result;
@@ -15,29 +16,36 @@ class _ExamScreenState extends State<ExamScreen> {
 
   Future<void> sendToGPT() async {
     // GPT API 요청을 위한 엔드포인트와 API 키 설정 (실제 사용하는 API에 따라 수정 필요)
-    final String endpoint = 'YOUR_GPT_API_ENDPOINT';
-    final String apiKey = 'YOUR_API_KEY';
+    final String endpoint = 'https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions';
+    final String apiKey = GPTApiKey;
 
-    // GPT에게 요청할 프롬프트 구성
-    String prompt = 'Summarize the following text: ${widget.result}';
+    // // GPT에게 요청할 프롬프트 구성
+    String prompt = '''아래 내용을 기반으로 시험 문제를 5개만 출제하여 json 구조로 줄바꿈 없이 돌려줘. 문제 유형에는 빈칸 채우기와 정답 고르기 유형이 있어. 수업 내용: ${widget.result}
+    json 구조: {"Topic": "시험 제목", "Question":[{"QNum": 문항 번호, "QType": "FillBlank", "QContent": 문제, "QAnswer": 정답}, {"QNum": 2, "QType": "SelectAnswer", "QContent": 문제, "QOptions":[{"OptionNum": 1, "OptionContent": 선택지 내용},{"OptionNum": 2,"OptionContent": 선택지 내용},{"OptionNum": 3, "OptionContent": 선택지 내용}, {"OptionNum": 4, "OptionContent": 선택지 내용}], "QAnswer": 정답 번호}]}
+    ''';
 
     // API 요청을 보내고 응답을 받아오는 부분
     try {
       final response = await http.post(
         Uri.parse(endpoint),
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json;charset=UTF-8',
           'Authorization': 'Bearer $apiKey',
         },
-        body: '{"prompt": "$prompt"}',
+        body: jsonEncode({
+          'prompt': prompt,
+          'max_tokens': 2500,
+          'temperature': 0.6,
+      }),
       );
 
       // 응답이 성공하면 결과물을 가져와서 상태 업데이트
       if (response.statusCode == 200) {
+        var data = jsonDecode(utf8.decode(response.bodyBytes));
         setState(() {
-          summaryResult = response.body;
+          summaryResult = data['choices'][0]['text'];
+          print(summaryResult);
         });
-      } else {
         // 응답이 실패한 경우에 대한 처리
         print('Failed to get response from GPT. Status code: ${response.statusCode}');
       }
@@ -57,21 +65,17 @@ class _ExamScreenState extends State<ExamScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Result from Upload: ${widget.result}',
-              style: TextStyle(fontSize: 18),
-            ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 // sendToGPT 함수 실행
                 sendToGPT();
               },
-              child: Text('Send to GPT'),
+              child: Text('시험 문제 불러오기'),
             ),
             SizedBox(height: 20),
             Text(
-              'Summary from GPT: $summaryResult',
+              '$summaryResult',
               style: TextStyle(fontSize: 18),
             ),
             // Add other widgets as needed
